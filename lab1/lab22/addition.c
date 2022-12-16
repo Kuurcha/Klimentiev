7f835c8b2a2da1533c283895af0b01513929f700
Kurcha <yhyr@mail.ru>
2022-12-16 20:07:11 +0400

    Header file added to share functions and constants across files

--- lab1/lab22/addition.c
+++ lab1/lab22/addition.c
@@ -2,14 +2,29 @@
 // Семафоры
 #include <sys/sem.h>
 #include <sys/ipc.h>
+
+
+//Константы и структура для операций
+#include <sharedLib.h>
 void* lab_add(struct OperationStructure** ptr){
     while (1){
         sem_wait(&add_sem);
         (*ptr)->originalValue +=(*ptr)->modificationValue;
-        sem_post(&main_sem);   
+ 
     }
 }
-main(){
+sem_t* main_sem;
+sem_t* add_sem;
+int main() {
     char* name = "lab1_shm\n\r";
     int shmDesc = shm_open(name, O_RDWR | O_CREAT,  READWRITE_PERMISSION);
+    
+    main_sem = createSem(main_sem, "Error while creating main sem", MAIN_SEMAPHOR);
+    add_sem = createSem(add_sem, "Error while creating main sem", ADD_SEMAPHOR);
+	
+	
+	while(1) {
+		add_process();
+	}
+	return 0;
 }
