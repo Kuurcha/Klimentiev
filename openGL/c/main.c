38ca7a95dacff1bb2d5f63d03eb3a78176abda11
Kurcha <yhyr@mail.ru>
2022-12-22 11:44:36 +0400

    Meow

--- openGL/c/main.c
+++ openGL/c/main.c
@@ -79,94 +79,161 @@ void display(void) {
  glMatrixMode (GL_PROJECTION);
 
 
-//  // Тело яйцо
+ // Тело яйцо
 
-//  glLoadIdentity(); 
-//  glColor3f(0.933, 0.933, 0.933); 
-//  glRotatef(45, -1, 1, 0);
-//  glScalef(1.8, 1.0, 0.6);
-//  glTranslatef (0.0,0.5, 0.0);
-//  gluSphere(QuadrObj,0.2, 20, 20);
-//  //Ноги
+ glLoadIdentity(); 
+ glColor3f(0.933, 0.933, 0.933); 
+ glRotatef(45, -1, 1, 0);
+ glScalef(1.8, 1.0, 0.6);
+ glTranslatef (0.0,0.5, 0.0);
+ gluSphere(QuadrObj,0.2, 20, 20);
+ //Ноги
  
-//  glLoadIdentity();
-//  glColor3f(0.8, 0.4, 0.0); 
-//  glTranslatef (0.0, 0.5, 0.0);
-//  glScalef(0.1, 0.13, 0.1);
-//  glRotatef(90, 1, 1, 1);
-//  glRotatef(-50, 1, 1, 0);
-//  gluCylinder(QuadrObj,0.5,0.5,16.0,30,30);
-
-//  glLoadIdentity();
-//  glTranslatef (0.25, -0.27, 0.0);
-//  glColor3f(0.8, 0.4, 0.0); 
-//  glScalef(1.0, 0.3, 1.0);
-//  glRotatef(25, 0.0,0.5,0.5);
-//  glutSolidCube(0.2);
-
- 
-//  glLoadIdentity();
-//  glColor3f(0.8, 0.4, 0.0); 
-//  glTranslatef (-0.2, 0.5, 0.0);
-//  glScalef(0.1, 0.13, 0.1);
-//  glRotatef(90, 1, 1, 1);
-//  glRotatef(-50, 1, 1, 0);
-//  gluCylinder(QuadrObj,0.5,0.5,16.0,30,30);
-
-//  glLoadIdentity();
-//  glTranslatef (0.05, -0.27, 0.0);
-//  glColor3f(0.8, 0.4, 0.0); 
-//  glScalef(1.0, 0.3, 1.0);
-//  glRotatef(25, 0.0,0.5,0.5);
-//  glutSolidCube(0.2);
-
-//  glLoadIdentity();
-//  glColor3f(0.933, 0.933, 0.933); 
-//  glTranslatef (-0.5, 0.8, 0.5);
-//  glScalef(0.2, 0.2, 0.2);
-//  glRotatef(90, 1, 1, 1);
-//  glRotatef(-50, 1, 1, 0);
-//  gluCylinder(QuadrObj,0.35,0.4,5.0,30,30);
-
-
-//  glLoadIdentity();
-//  glColor3f(0.933, 0.933, 0.933); 
-//  glScalef(1.1, 0.8, 1.0);
-//  glTranslatef (-0.5, 1.0, 0.5);
-//  glRotatef(-30, 0, 1, 1);
-//  glutSolidSphere(0.2, 30, 30);
- 
-//  glLoadIdentity();
-//  glColor3f(0.8, 0.4, 0.0);
-//  glTranslatef (-0.58, 0.75, 0.3);
-//  glRotatef(-90, 0, 1, -1);
- 
-//  glRotatef(-80, 1, 1, 0);
-//  glutSolidCone(0.05, 0.4, 30, 30);
-
-
-//  //глаза
-
-//  glLoadIdentity();
-//  glColor3f(1, 1, 1);
-//  glTranslatef (-0.5, 0.8, 0.0);
-//  glutSolidSphere(0.05, 30, 30);
-//  glutSwapBuffers();  
-
-//   glLoadIdentity();
-//  glColor3f(1, 1, 1);
-//  glTranslatef (-0.7, 0.8, -0.7);
-//  glutSolidSphere(0.05, 30, 30);
-//  glutSwapBuffers();  
-
- //крылья
-
+ glLoadIdentity();
+ glColor3f(0.8, 0.4, 0.0); 
+ glTranslatef (0.0, 0.5, 0.0);
+ glScalef(0.1, 0.13, 0.1);
+ glRotatef(90, 1, 1, 1);
+ glRotatef(-50, 1, 1, 0);
+ gluCylinder(QuadrObj,0.5,0.5,16.0,30,30);
 
  glLoadIdentity();
-  glColor3f(0.3, 0.4, 0.6); 
- glTranslatef (0.5, 0.5, 0.0);
- glutSolidCube(0.4);
+ glTranslatef (0.25, -0.27, 0.0);
+ glColor3f(0.8, 0.4, 0.0); 
+ glScalef(1.0, 0.3, 1.0);
+ glRotatef(25, 0.0,0.5,0.5);
+ glutSolidCube(0.2);
 
+ 
+ glLoadIdentity();
+ glColor3f(0.8, 0.4, 0.0); 
+ glTranslatef (-0.2, 0.5, 0.0);
+ glScalef(0.1, 0.13, 0.1);
+ glRotatef(90, 1, 1, 1);
+ glRotatef(-50, 1, 1, 0);
+ gluCylinder(QuadrObj,0.5,0.5,16.0,30,30);
+
+ glLoadIdentity();
+ glTranslatef (0.05, -0.27, 0.0);
+ glColor3f(0.8, 0.4, 0.0); 
+ glScalef(1.0, 0.3, 1.0);
+ glRotatef(25, 0.0,0.5,0.5);
+ glutSolidCube(0.2);
+
+ //шея
+ glLoadIdentity();
+ glColor3f(0.933, 0.933, 0.933); 
+ glTranslatef (-0.4, 0.8, 0.5);
+ glScalef(0.2, 0.2, 0.2);
+ glRotatef(90, 1, 1, 1);
+ glRotatef(-50, 1, 1, 0);
+ gluCylinder(QuadrObj,0.35,0.4,5.0,30,30);
+
+
+ //голова
+ glLoadIdentity();
+ glColor3f(0.933, 0.933, 0.933); 
+ glScalef(1.1, 0.8, 1.0);
+ glTranslatef (-0.33, 1.05, 0.5);
+ glRotatef(-30, 0, 1, 1);
+ glutSolidSphere(0.2, 30, 30);
+ 
+ glLoadIdentity();
+ glColor3f(0.8, 0.4, 0.0);
+ glTranslatef (-0.40, 0.80, 0.3);
+ glRotatef(-90, 0, 1, -1);
+ 
+ glRotatef(-80, 1, 1, 0);
+ glutSolidCone(0.05, 0.4, 30, 30);
+
+
+ //глаза
+
+ glLoadIdentity();
+ glColor3f(1, 1, 1);
+ glTranslatef (-0.3, 0.85, 0.0);
+ glutSolidSphere(0.05, 30, 30);
+ glutSwapBuffers();  
+
+  glLoadIdentity();
+ glColor3f(1, 1, 1);
+ glTranslatef (-0.5, 0.85, -0.7);
+ glutSolidSphere(0.05, 30, 30);
+
+
+//крылья
+
+  glLoadIdentity();
+  glColor3f(0.933, 0.933, 0.933);
+  glTranslatef (0.5, 0.5, 0.0); 
+  glScalef(2.0, 0.4, 0.5);
+  glRotatef(135, 1, 1, 1);
+  glutSolidCube(0.4);
+
+  glLoadIdentity();
+  glColor3f(0.933, 0.933, 0.933);
+  glTranslatef (-0.55, 0.4, 0.1); 
+  glScalef(2.0, 0.4, 0.5);
+  glRotatef(135, 1, 1, 1);
+  glRotatef(12, 0, 1, 1);
+  glutSolidCube(0.4);
+  
+  glLoadIdentity();
+  glColor3f(0.5333, 0.364, 0.682); 
+  glTranslatef (-0.1, -0.03, 0.5); 
+  glScalef(0.8, 1.5, 1.0);
+  glRotatef(90, 1, 1, 1);
+  glRotatef(-50, 1, 1, 0);
+  gluCylinder(QuadrObj,0.5,0.5,0.12,30,30);
+
+
+  //Ребенок
+
+ glLoadIdentity(); 
+ glColor3f(0.970, 0.970, 0.970); 
+ glTranslatef (-0.1,-0.5, 0.0);
+ glRotatef(45, -1, 1, 1);
+ glScalef(1.2, 0.69, 0.6);
+
+ gluSphere(QuadrObj,0.25, 20, 20);
+
+ glLoadIdentity(); 
+ glColor3f(0.886, 0.721, 0.705); 
+ glTranslatef (-0.16, -0.48, -0.3);
+ glRotatef(78, 0, 1, 1);
+ glScalef(0.5, 1.0, 1.0);
+
+ gluSphere(QuadrObj,0.1, 20, 20);
+
+
+    //глаза ребенка
+
+     //глаза
+
+ glLoadIdentity();
+
+ glColor3f(1, 1, 1);
+ glTranslatef (-0.22, -0.46, -0.5);
+ glRotatef(78, 0, 1, 1);
+ glutSolidSphere(0.02, 30, 30);
+
+ glLoadIdentity();
+ glColor3f(1, 1, 1);
+ glTranslatef (-0.11, -0.51, -0.5);
+  glRotatef(78, 0, 1, 1);
+ glutSolidSphere(0.02, 30, 30);
+
+    //соска
+    glLoadIdentity();
+    glColor3f(0.1, 0.1, 0.8);
+        glTranslatef (-0.1, -0.41, -0.3);
+    glRotatef(90, 0, -1, 1);
+
+
+    glutSolidCone(0.05, 0.2, 30, 30);
+
+
+ glutSwapBuffers();  
  glFlush(); 
 } 
 
